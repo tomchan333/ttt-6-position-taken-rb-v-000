43c25eb83f76450c2dd8f2e8d5c64077748876c9
Tom Chan <tomchanthree@gmail.com>
2017-10-27 21:04:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,8 @@
 def position_taken? (board, index)
   if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
   elsif board[index] == nil
     return false
   elsif board[index] == "X"
